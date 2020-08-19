d2a111f0e5b49a78fb81f2ba43a3af91ed86f894
Nguyen Viet Tien <38419585+viettiennguyen029@users.noreply.github.com>
2020-08-19 09:07:30 +0700

    Initial Commit

---
+++ Email Login Page/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Email Login Page
+//
+//  Created by Nguyen Viet Tien on 8/19/20.
+//  Copyright © 2020 Nguyen Viet Tien. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
