fc59c8d4adaea7e640c9968f8f99c3f5104bb52f
Nguyen Viet Tien <38419585+viettiennguyen029@users.noreply.github.com>
2020-08-19 17:00:27 +0700

    Add Sign Up Page

--- Email Login Page/ContentView.swift
+++ Email Login Page/ContentView.swift
@@ -23,14 +23,30 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct Home: View {
+    
+    @State var show = false
+    
     var body: some View{
-        VStack{
-            Login()
-        }
+        
+//        NavigationView{
+//            ZStack{
+//                NavigationLink(destination: SignUp(show: self.$show), isActive: self.$show){
+//                    Text("")
+//                }.hidden()
+//
+//                Login(show: self.$show)
+//            }
+//        }
+//        .navigationBarTitle("")
+//        .navigationBarHidden(true)
+//        .navigationBarBackButtonHidden(true)
+        Login(show: self.$show)
     }
+    
 }
 
 struct Login: View{
+    @Binding var show: Bool
     @State var email = ""
     @State var pass = ""
     @State var color = Color.black.opacity(0.7)
@@ -39,8 +55,8 @@ struct Login: View{
     let borderColor = Color(red: 107.0/255.0, green: 164.0/255.0, blue: 252.0/255.0)
     
     var body: some View{
-        VStack{
-            Image("finance_app").resizable().frame(width: 300.0, height: 255.0, alignment: .center)
+        VStack(){
+            Image("finance_app").resizable().frame(width: 300.0, height: 255.0, alignment: .top)
             
             Text("Log in to your account")
                 .font(.title)
@@ -90,6 +106,7 @@ struct Login: View{
                 }.padding(.top, 10.0)
             }
             
+            // Sign in button
             Button(action: {
                 // Insert
             }) {
@@ -102,7 +119,147 @@ struct Login: View{
             .background(Color("Dominant"))
             .cornerRadius(6)
             .padding(.top, 15)
+            
+            HStack(spacing: 5){
+                Text("Don't have an account ?")
+                
+                Button(action: {
+                    // Navigate to sign up page
+                    self.show.toggle()
+                }) {
+                    Text("Sign up")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("Dominant"))
+                    
+                }
+                Text("now").multilineTextAlignment(.leading)
+                
+            }.padding(.top, 25)
+
         }
         .padding(.horizontal, 25)
+    
+    }
+}
+
+struct SignUp: View{
+    
+    @Binding var show : Bool
+    @State var email = ""
+    @State var pass = ""
+    @State var repass = ""
+    
+    @State var color = Color.black.opacity(0.7)
+    
+    @State var visible = false
+    @State var revisible = false
+    
+    let borderColor = Color(red: 107.0/255.0, green: 164.0/255.0, blue: 252.0/255.0)
+    
+    var body: some View{
+        
+        ZStack(alignment: .topLeading){
+            
+            GeometryReader{_ in
+                
+                VStack{
+                    Image("finance_app").resizable().frame(width: 300.0, height: 255.0, alignment: .center)
+                    
+                    Text("Sign up a new account")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(self.color)
+                        .padding(.top, 15)
+                    
+                    TextField("Username or Email",text:self.$email)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius:6).stroke(self.borderColor,lineWidth:2))
+                        .padding(.top, 0)
+                    
+                    HStack(spacing: 15){
+                        VStack{
+                            if self.visible {
+                                TextField("Password", text: self.$pass)
+                                    .autocapitalization(.none)
+                            } else {
+                                SecureField("Password", text: self.$pass)
+                                    .autocapitalization(.none)
+                            }
+                        }
+                        
+                        Button(action: {
+                            self.visible.toggle()
+                        }) {
+                            //Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+                            Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                .foregroundColor(self.color)
+                                .opacity(0.8)
+                        }
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 6)
+                    .stroke(self.borderColor,lineWidth: 2))
+                    .padding(.top, 10)
+                    
+                    
+                    // Confirm password
+                    HStack(spacing: 15){
+                        VStack{
+                            if self.revisible {
+                                TextField("Confirm Password", text: self.$repass)
+                                    .autocapitalization(.none)
+                            } else {
+                                SecureField("Confirm Password", text: self.$repass)
+                                    .autocapitalization(.none)
+                            }
+                        }
+                        
+                        Button(action: {
+                            self.revisible.toggle()
+                        }) {
+                            //Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+                            Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                .foregroundColor(self.color)
+                                .opacity(0.8)
+                        }
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 6)
+                    .stroke(self.borderColor,lineWidth: 2))
+                    .padding(.top, 10)
+                    
+                    
+                    // Sign in button
+                    Button(action: {
+                        // Insert validate func
+                    }) {
+                        Text("Sign up")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width - 50)
+                    }
+                    .background(Color("Dominant"))
+                    .cornerRadius(6)
+                    .padding(.top, 15)
+                    
+                }
+                .padding(.horizontal, 25)
+                
+            }
+            
+            Button(action: {
+                self.show.toggle()
+            }) {
+                Image(systemName: "chevron.left")
+                    .font(.title)
+                    .foregroundColor(Color("Dominant"))
+            }
+
+        }
+        .navigationBarBackButtonHidden(true)
+        
+        
     }
 }
