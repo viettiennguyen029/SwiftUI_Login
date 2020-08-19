95678dbbb7636a41ce10b2a2d0faa382c3be5fc7
Nguyen Viet Tien <38419585+viettiennguyen029@users.noreply.github.com>
2020-08-19 11:19:53 +0700

    Implement login page UI

--- Email Login Page/ContentView.swift
+++ Email Login Page/ContentView.swift
@@ -11,12 +11,98 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Email Login Page Project, this is dev branch")
+        //Text("Email Login Page Project, this is dev branch")
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View{
+        VStack{
+            Login()
+        }
+    }
+}
+
+struct Login: View{
+    @State var email = ""
+    @State var pass = ""
+    @State var color = Color.black.opacity(0.7)
+    @State var visible = false
+    
+    let borderColor = Color(red: 107.0/255.0, green: 164.0/255.0, blue: 252.0/255.0)
+    
+    var body: some View{
+        VStack{
+            Image("finance_app").resizable().frame(width: 300.0, height: 255.0, alignment: .center)
+            
+            Text("Log in to your account")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(self.color)
+                .padding(.top, 15)
+            
+            TextField("Username or Email",text:self.$email)
+                .autocapitalization(.none)
+                .padding()
+                .background(RoundedRectangle(cornerRadius:6).stroke(borderColor,lineWidth:2))
+                .padding(.top, 0)
+            
+            HStack(spacing: 15){
+                VStack{
+                    if self.visible {
+                        TextField("Password", text: self.$pass)
+                            .autocapitalization(.none)
+                    } else {
+                        SecureField("Password", text: self.$pass)
+                            .autocapitalization(.none)
+                    }
+                }
+                
+                Button(action: {
+                    self.visible.toggle()
+                }) {
+                    //Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+                    Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                        .foregroundColor(self.color)
+                        .opacity(0.8)
+                }
+            }
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 6)
+            .stroke(borderColor,lineWidth: 2))
+            .padding(.top, 10)
+            
+            HStack{
+                Spacer()
+                Button(action: {
+                    self.visible.toggle()
+                }) {
+                    Text("Forget Password")
+                        .fontWeight(.medium)
+                        .foregroundColor(Color("Dominant"))
+                }.padding(.top, 10.0)
+            }
+            
+            Button(action: {
+                // Insert
+            }) {
+                Text("Sign in")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                 .frame(width: UIScreen.main.bounds.width - 50)
+            }
+            .background(Color("Dominant"))
+            .cornerRadius(6)
+            .padding(.top, 15)
+        }
+        .padding(.horizontal, 25)
     }
 }
